62b609e972b05dc8ea39e5cf24aa7a340ae8c2b5
dulce valeria guzman taveras <Blastomm@hotmail.com>
2021-11-29 13:34:01 -0400

    avances ejercicio 1

--- src/views/Ejercicio1.tsx
+++ src/views/Ejercicio1.tsx
@@ -1,9 +1,19 @@
 import React from "react";
+import Box from '@mui/material/Box';
+import FormControl from '@mui/material/FormControl';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import { Button } from '@mui/material';
 
 const Ejercicio1 = () => {
     return(
         <div className="class-Ejercicio1">
-            Ejercicio1
+            <Box component="form" noValidate autoComplete="off">
+                <FormControl sx={{ width: '25ch' }}>
+                <OutlinedInput type="number" placeholder="write a number"/>
+                <OutlinedInput type="number" placeholder="write a number"/>
+                    <Button variant="contained">find prime numbers</Button>
+                </FormControl>
+            </Box>
         </div>
     )
 }
